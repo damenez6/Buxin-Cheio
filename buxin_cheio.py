1ffa7241d600d5a1ef29a0a24946e320c27a0720
SerranoZz <78428905+SerranoZz@users.noreply.github.com>
2021-07-12 11:27:41 -0300

    Add files via upload

---
+++ buxin_cheio.py
@@ -0,0 +1,28 @@
+from pygame.mixer import pause, stop
+from PPlay.mouse import Mouse
+from pygame.event import wait
+from PPlay.animation import*
+from typing import*
+from PPlay.window import*
+from PPlay.gameimage import*
+from PPlay.sprite import*
+
+#GameLoop
+while(True):
+    #Entrada de dados
+   
+   
+
+    #Update dos Game Objects
+    
+ 
+
+    
+    #Physics
+    
+        
+    
+
+    #Desenho dos Game Objects
+    
+#Encerramento
